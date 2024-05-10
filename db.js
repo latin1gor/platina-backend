dc6795b38862dfd0a5b33fba758ea3766388dd06
latin1gor <latinigor.main@gmail.com>
2024-05-10 15:17:04 +0300

    vercel

--- db.js
+++ db.js
@@ -1,12 +1,4 @@
 import { Sequelize } from "sequelize";
 
-export default new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  },
+export default new Sequelize(process.env.POSTGRES_URL
 );
